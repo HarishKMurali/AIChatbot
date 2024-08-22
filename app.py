8cdb941070d9450ed37194587ab939fa2d8890e5
HarishKMurali <harishkmurali2000@gmail.com>
2024-08-22 19:40:43 -0400

    port change

--- app.py
+++ app.py
@@ -57,4 +57,6 @@ def get_bot_response():
     return bot_response
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
